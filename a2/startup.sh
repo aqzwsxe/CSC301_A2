08678c602e11404cdccb5f1294d14d8c0244204c
zhengci1 <cindyxp.zheng@mail.utoronto.ca>
2026-03-10 14:13:42 -0400

    Added startup.sh

---
+++ a2/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="Desktop/301/A2_component2/CSC301_A2/a2"
+
+machines=(
+  dh2026pc06
+  dh2026pc07
+  dh2026pc09
+  dh2026pc10
+  dh2026pc11
+)
+
+./runme.sh -c
+
+for m in "${machines[@]}"; do
+    echo "Launching on $m"
+    ssh $m "cd $DIR && ./launch_script.sh" &
+done
+
+wait
+echo "All machines started."
