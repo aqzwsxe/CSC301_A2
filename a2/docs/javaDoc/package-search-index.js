8db070b55c458eb1889d5357ff3849d439ac8b68
aqzwsxe <fzh200393@gmail.com>
2026-03-11 11:20:23 -0400

    remove the old docs

--- a2/docs/javaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"ISCS"},{"l":"OrderService"},{"l":"ProductService"},{"l":"UserService"},{"l":"Utils"}];updateSearchResults();
